7e72b6f1f49ac8ba297a4b7e849f44d9a74cbad0
JosephMA7 <jjmartinez10@utpl.edu.ec>
2024-04-29 20:01:31 -0500

    commit

---
+++ ejemplo02.py
@@ -0,0 +1,6 @@
+mensaje = "Hola mundo"
+
+for x in range (0,10):
+	print("%s" % (mensaje))
+	
+print("Fin del mensaje")
---
+++ ejemplo04.py
@@ -0,0 +1,10 @@
+sueldo =int(input("ingrese el sueldo: "))
+sueldo = 100
+if sueldo <= 100:
+	print("correcto")
+else:
+	if(sueldo >= 101) and (sueldo <= 110):
+		print("Sobresaliente")
+
+	else:
+		print("incorrecto")
